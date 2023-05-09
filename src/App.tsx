cd8eebd3a19d781680bc27f9a2758eb2ecb8d8ff
pgw6541 <pgw6541@gmail.com>
2023-05-09 23:36:40 +0900

    Project Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import { AllCarsPage } from './pages/AllCarsPage'
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar bg="dark" variant='dark'>
+        <Container>
+          <Nav>
+            <Nav.Link onClick={()=>{ navigate('/')}}>Main</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path={'/'} element={<AllCarsPage />}></Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
