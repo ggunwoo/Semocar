ba7b68d1b8014b3293a1ec0788a334470ede9d88
pgw6541 <pgw6541@gmail.com>
2023-05-28 16:30:21 +0900

    error message fix

--- src/api/getCarData.ts
+++ src/api/getCarData.ts
@@ -5,7 +5,7 @@ export const getCarData = async () => {
     const response = await axios.get('https://gist.githubusercontent.com/pgw6541/9db3dd7dc7fe2c28d1c529e47b7d062b/raw/5878bf8542383ad4c2e0bda6dbe19fdcb9d8ca9b/carData.json');
     return response.data;
   } catch (error) {
-    console.log('error carinfo', error);
+    console.log('error car data', error);
     return null;
   }
 };
@@ -15,7 +15,7 @@ export const getCarBrands = async () => {
     const response = await axios.get('https://gist.githubusercontent.com/pgw6541/9db3dd7dc7fe2c28d1c529e47b7d062b/raw/5878bf8542383ad4c2e0bda6dbe19fdcb9d8ca9b/brand.json');
     return response.data;
   } catch (error) {
-    console.log('error brand', error);
+    console.log('error brand data', error);
     return null;
   }
 };
