35aaa039b163a6555d36e1ce1333937693f3a11b
pgw6541 <pgw6541@gmail.com>
2023-05-30 02:48:57 +0900

    getCarData URL fix

--- src/api/getCarData.ts
+++ src/api/getCarData.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getCarData = async () => {
   try {
-    const response = await axios.get('https://gist.githubusercontent.com/pgw6541/9db3dd7dc7fe2c28d1c529e47b7d062b/raw/5878bf8542383ad4c2e0bda6dbe19fdcb9d8ca9b/carData.json');
+    const response = await axios.get('https://gist.githubusercontent.com/pgw6541/9db3dd7dc7fe2c28d1c529e47b7d062b/raw/f4924714350fa9e8c59951bc70ef321b9f5b4af5/carData.json');
     return response.data;
   } catch (error) {
     console.log('error car data', error);
