2f0adbe68b2b1c0cf78b5e3db106d9ad161ad080
pgw6541 <96gunw@gmail.com>
2023-11-01 18:37:38 +0900

    Update api

--- src/api/getCarData.ts
+++ src/api/getCarData.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getCarData = async () => {
   try {
-    const response = await axios.get('https://gist.githubusercontent.com/pgw6541/3bec1be58457d14aab3e04fea8434458/raw/0e6dec4900abfeeb9e605940827131613b0388a6/CarData.json');
+    const response = await axios.get('https://gist.githubusercontent.com/pgw6541/3bec1be58457d14aab3e04fea8434458/raw/489ee75632f5d987cbc6e423bacce6ec8af481d6/CarData.json');
     return response.data;
   } catch (error) {
     console.log('error car data', error);
