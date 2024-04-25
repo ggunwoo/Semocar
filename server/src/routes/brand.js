010f34105ffd7792a7b0e35e7475fdcd188cf9c3
pgw6541 <pgw6541@gmail.com>
2024-04-25 22:37:07 +0900

    MAKE | server - TS -> JS

--- server/src/routes/brand.js
+++ server/src/routes/brand.js
@@ -1,10 +1,10 @@
-import express, { Request, Response }  from "express";
-import Brand from "../models/brand"; // Model
+import express  from "express";
+import Brand from "../models/brand.js"; // Model
 
 const router = express.Router();
 
 // 브랜드 생성 POST 요청
-router.post('/create/brand', async (req: Request, res:Response) => {
+router.post('/create/brand', async (req, res) => {
   try {
     const { name, english_name, id, logo_path} = req.body;
     const newBrand = new Brand({
@@ -18,7 +18,7 @@ router.post('/create/brand', async (req: Request, res:Response) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
     res.write("<script>alert('전송완료')</script>");
     res.write("<script>window.location=\"/create\"</script>");
-} catch (error: any) {
+} catch (error) {
     res.write(`<script>alert('전송실패' ${error})</script>`);
     res.status(500).send(error.message);
 }
