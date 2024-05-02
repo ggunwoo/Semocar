bec3e780a503826a3ea598230485a61825b96de5
pgw6541 <pgw6541@gmail.com>
2024-05-02 23:23:42 +0900

    Fix | status 추가

--- server/src/routes/createCarRoute.js
+++ server/src/routes/createCarRoute.js
@@ -11,7 +11,6 @@ router.post("/cars", async (req, res) => {
 
     res.status(201).send("전송완료");
   } catch (error) {
-    res.send("전송실패");
     res.status(400).send( "전송실패: " + error.message );
     // TODO : DB에 같은 ID값이 존재할 시 state(400) 요청 및 같은 ID값이 존재하다는 메세지 반환
   }
