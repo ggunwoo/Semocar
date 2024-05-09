329176c51bebda006ec0c7e939260479ae53563c
pgw6541 <pgw6541@gmail.com>
2024-05-10 02:52:39 +0900

    Update | id값 시퀀스 번호 할당

--- server/src/routes/createCarRoute.js
+++ server/src/routes/createCarRoute.js
@@ -3,11 +3,34 @@ import Car from "../models/car.js";
 
 const router = express.Router();
 
+// --시퀀스 번호를 추가한 최종 ID 생성 함수
+async function generateSequenceId(baseId) {
+  let sequence = 1;
+  let finalId = "";
+
+  // --DB에 중복된 ID가 없을 때까지 시퀀스 번호를 증가
+  do {
+    finalId = `${baseId}${sequence}`;
+    const existingCar = await Car.findOne({ id: finalId });
+    if (!existingCar) break;
+    sequence += 1;
+  } while (sequence <= 9);
+
+  if (sequence > 9) throw new Error("ID 시퀀스 번호가 9를 초과했습니다.");
+
+  return finalId;
+}
+
 router.post("/cars", async (req, res) => {
   try {
-    console.log(req.body);
-    const car = new Car(req.body);
-    await car.save();
+    const { id: baseId, ...otherCarData } = req.body;
+    console.log("req.body : ", req.body);
+
+    const finalId = await generateSequenceId(baseId);
+
+    const newCar = new Car({ id: finalId, ...otherCarData });
+    console.log("newCar : ", newCar);
+    await newCar.save();
 
     res.status(201).send("전송완료");
   } catch (error) {
