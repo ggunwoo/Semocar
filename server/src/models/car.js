5196d1d54d7aa62364625a1e59ef6a9eef858773
pgw6541 <pgw6541@gmail.com>
2024-05-09 20:21:56 +0900

    Fix | 자동차 모델 수정

--- server/src/models/car.js
+++ server/src/models/car.js
@@ -2,24 +2,18 @@ import mongoose from "mongoose";
 
 // 트림(Trims) 스키마
 const trimSchema = new mongoose.Schema({
-  //■■■■■전 차종 Trim 기본 필드■■■■■
-  // name: { type: String, required: true }, // 이름
-  id: Number, // 고유값 id할당 ▶ 오름차순 정렬을 위한
+  // ■■■■■전 차종 Trim 기본 필드■■■■■
   name: String, // 이름
+  id: Number, // 고유값 id할당 ▶ 오름차순 정렬을 위한
+  field: String, // 트림 연료 유형 ▶ ICE(내연기관), HEV, PHEV(하이브리드), EV(전기)
   price: Number, // 가격
-  // fuel_type: { name: String, id: Number }, // 연료 ▶ 가솔린(1), 디젤(1), 하이브리드(2), 전기,수소(3)
-  fuel_type: String, // 연료 ▶ 가솔린(1), 디젤(1), 하이브리드(2), 전기,수소(3)
-  // engine: { type: String, required: true }, // 엔진 종류 ▶ 직렬, v6 등
-  engine: String, // 엔진 종류 ▶ 직렬, v6 등
-  displacement: String, // 배기량 ▶ ----cc
-  trans_mission: String, // 변속기 ▶ 수동, 8단자동, 7단DCT, CVT 등
-  driving_system: String, // 구동타입 ▶ FF, FR, RR, 4WD 등
-  power: Number, // 출력 ▶ ---마력
-  torque: Number, // 토크 ▶ --.-kg.m
-  gas_mileage: String, // 복합연비 ▶ --.-km/l
-  urban_gas_mileage: Number, // 도심연비 ▶ --.-km/l
-  highway_gas_mileage: Number, // 고속연비 ▶ --.-km/l
-  low_emission: Number, // 에너지 소비 효율 ▶ -등급
+  fuel_type: String, // 연료 ▶ 가솔린, 디젤, 하이브리드, 전기, 수소
+  low_emission: String, // 에너지 소비 효율 ▶ -등급
+  trans_mission: {
+    gear: String, // 변속기단수 ▶ 1단~8단 (일단 8단까지만)
+    type: String, // 변속기종류 ▶ 수동, 토크컨버터, DCT, CVT, AMT, IVT 등
+  }, 
+  driving_system: String, // 구동타입 ▶ FF, FR, RR, 4WD AWD 등
   vehicle_weight: Number, // 공차중량 ▶ ----kg
   front_tire: String, // 앞 타이어 규격 ▶ --- --R --
   rear_tire: String, // 뒷 타이어 규격 ▶ --- --R --
@@ -34,18 +28,29 @@ const trimSchema = new mongoose.Schema({
   wheel_base: Number, // 축거(휠베이스) ▶ ----mm
   track: Number, // 윤거전 ▶ ----mm
   tread: Number, // 윤거후 ▶ ----mm
+  
+  // ■■■■■내연기관(ICE), 하이브리드(HEV, PHEV) 추가 필드■■■■■
+  engine: String, // 엔진 종류 ▶ 직렬, v6 등
+  power: Number, // 출력 ▶ ---마력
+  torque: Number, // 토크 ▶ --.-kg.m
+  displacement: Number, // 배기량 ▶ ----cc
+  gas_mileage: Number, // 복합연비 ▶ --.-km/l
+  urban_gas_mileage: Number, // 도심연비 ▶ --.-km/l
+  highway_gas_mileage: Number, // 고속연비 ▶ --.-km/l
 
-  //■■■■■하이브리드, 전기(수소) Trim 추가 필드■■■■■
+  // ■■■■■하이브리드(HEV, PHEV), 전기(EV(배터리, 수소)) 추가 필드■■■■■
   motor_power: Number, // 모터출력 ▶ ---kw
   motor_torque: Number, // 모터토크 ▶ --.-kg.m
-  battery_type: Number, // 배터리 종류 ▶ 리튬이온, 리튬 인산철 등
-  battery_volume: Number, // 배터리 용량 ▶ kWh
-  battery_voltage: Number,
-
-  //■■■■■플러그인 하이브리드, 전기(수소) Trim 고유 필드■■■■■
+  
+  // ■■■■■플러그인 하이브리드(PHEV), 전기(EV(배터리, 수소)) 고유 필드■■■■■
+  battery_type: String, // 배터리 종류 ▶ 리튬이온, 리튬 인산철 등
+  battery_volume: Number, // 배터리 용량 ▶ --.- kWh
   ev_mileage: Number, // 복합전비 ▶ ---km/kWh
-  urban_ev_mileage: Number, // 도심전비 ▶ ---km/kWh
+  urban_ev_mileage: Number, // 도심전비 ▶ ---km/kcdWh
   highway_ev_mileage: Number, // 고속전비 ▶ ---km/kWh
+  
+  // ■■■■■전기(EV(배터리, 수소)) Trim 고유 필드■■■■■
+  battery_voltage: Number, // 배터리 전압 ▶ -- V
   range: Number, // 복합 주행거리 ▶ ---km/kWh
   urban_range: Number, // 도심 주행거리 ▶ ---km/kWh
   highway_range: Number, // 고속 주행거리 ▶ ---km/kWh
@@ -53,7 +58,7 @@ const trimSchema = new mongoose.Schema({
 
 // 모델(Grades) 스키마
 const gradeSchema = new mongoose.Schema({
-  //■■■■■ Grades 필드■■■■■
+  // ■■■■■ Grades 필드■■■■■
   name: String, // Grades명 ▶ 프리미엄, 익스클루시브 등
   id: Number, // Grades ID(고유값) ▶ 0 ~ 99
   trims: [trimSchema], // Trim스키마 참조
@@ -61,7 +66,7 @@ const gradeSchema = new mongoose.Schema({
 
 // 기본 제원 스키마
 const baseCarSchema = new mongoose.Schema({
-  //■■■■■ 기본 제원 필드■■■■■
+  // ■■■■■ 기본 제원 필드■■■■■
   brand: { type: mongoose.Schema.Types.ObjectId, ref: "Brands" }, // ref: 브랜드 콜렉션 참조
   name: String, // 차량 모델명 ▶ 쏘나타, 콜로라도, GV90 등
   english_name: String, // 차량 모델명(영어) ▶ Sonata, Colorado, GV90
@@ -82,7 +87,7 @@ const baseCarSchema = new mongoose.Schema({
   gas_mileage: {
     min: Number,
     max: Number,
-  }, // 연비 ▶ "--.-~--.-" km/l
+  }, // 연비 ▶ "--.-~--.-" km/l(ICE, HEV) or kWh(EV)
   fuel_types: [{
     name: String,
     id: Number
--- server/src/routes/createCarRoute.js
+++ server/src/routes/createCarRoute.js
@@ -1,17 +1,17 @@
 import express from "express";
 import Car from "../models/car.js";
 
-
 const router = express.Router();
 
 router.post("/cars", async (req, res) => {
   try {
+    console.log(req.body);
     const car = new Car(req.body);
     await car.save();
 
     res.status(201).send("전송완료");
   } catch (error) {
-    res.status(400).send( "전송실패: " + error.message );
+    res.status(400).send("전송실패: " + error.message);
     // TODO : DB에 같은 ID값이 존재할 시 state(400) 요청 및 같은 ID값이 존재하다는 메세지 반환
   }
 });
