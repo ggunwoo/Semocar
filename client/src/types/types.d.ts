33d49069e6beafebce86cdd5f465c87cdb2d3e34
pgw6541 <pgw6541@gmail.com>
2024-05-09 20:20:52 +0900

    Fix | 기본 Form Type 삭제

--- client/src/types/types.d.ts
+++ client/src/types/types.d.ts
@@ -1,17 +1,57 @@
-export interface Trims {
-  name: string;
+export interface TrimType {
   id: number;
+  name: string;
+  price: number;
+  field?: string;
+  engine?: string;
   fuel_type: string;
+  displacement?: string;
+  trans_mission: {
+    type: string,
+    gear: string,
+  };
+  driving_system: string;
+  power?: number;
+  torque?: number;
+  gas_mileage?: string;
+  urban_gas_mileage?: number;
+  highway_gas_mileage?: number;
+  low_emission: string;
+  vehicle_weight: number;
+  front_tire: string;
+  rear_tire: string;
+  front_brake: string;
+  rear_brake: string;
+  front_suspension: string;
+  rear_suspension: string;
+  capacity: number;
+  length: number;
+  weight: number;
+  height: number;
+  wheel_base: number;
+  track: number;
+  tread: number;
+  motor_power?: number;
+  motor_torque?: number;
+  battery_type?: number;
+  battery_volume?: number;
+  battery_voltage?: number;
+  ev_mileage?: number;
+  urban_ev_mileage?: number;
+  highway_ev_mileage?: number;
+  range?: number;
+  urban_range?: number;
+  highway_range?: number;
 }
 
-export interface Grades {
+export interface GradeType {
   name: string;
   id: number;
   trims: trim[];
 }
 
-//■■ 자동차 제원 데이터 서버 전송 타입 ■■
-export interface CarData_Type {
+// ■■ 자동차 제원 데이터 서버 전송 타입 ■■
+export interface CarDataType {
   brand: string;
   name: string;
   english_name: string;
@@ -24,6 +64,13 @@ export interface CarData_Type {
   fuel_types: { name: string; id: number }[];
   grades: grade[];
 }
+
+
+
+
+
+
+// ==========Old========
 
 // brand.json (Old)
 export interface Brands {
