a84762dda7c418f0df1e5a7bbce9fb61e1230e4e
pgw6541 <pgw6541@gmail.com>
2024-05-29 17:12:29 +0900

    Fix | SelectBox 전반적인 수정 작업

--- client/src/components/search/SelectBox.tsx
+++ client/src/components/search/SelectBox.tsx
@@ -1,34 +1,50 @@
-import "../../styles/test.scss"
+import "../../styles/test.scss";
 
 import { useAppSelector, useAppDispatch } from "../../store/hooks";
-import {SEGMENT_LIST, FUELTYPE_LIST} from "../../../utils/constants"
+import { SEGMENT_LIST, FUELTYPE_LIST } from "../../../utils/constants";
 import { useNavigate } from "react-router-dom";
 
-import SelectBrand from "../search/SelectBrand"
-
+import SelectBrand from "../search/SelectBrand";
 
 export default function SelectBox() {
   const dispatch = useAppDispatch;
   const navigate = useNavigate();
 
-  const brands = useAppSelector(state =>  state.brandList.items);
+  const brands = useAppSelector(state => state.brandList.items);
 
   return (
     <section className="container-select-box">
-      <article className={`${"select-line"} brand-list`}>
-        {
-          brands.map((brand)=>(
-            <div className="brand-item">
+      <article className="select-line">
+        <h2>브랜드</h2>
+        <ul className="list brand-list">
+          {brands.map(brand => (
+            <li className="brand-item">
               <div className="logo">
                 <img className="logo-img" src={`${brand.logo_path}`} />
               </div>
               <p className="name">{brand.name}</p>
-            </div>
-          ))
-        }
+            </li>
+          ))}
+        </ul>
       </article>
-      <article className={`select-line seg-line`}></article>
-      <article className={`select-line fuel-line`}></article>
+      <article className="select-line ">
+        <h2>차급</h2>
+        <ul className="list seg-list">
+          {SEGMENT_LIST.map((segment, idx) => (
+            <li key={idx}>
+              <p>{segment}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+      <article className="select-line ">
+        <h2>연료</h2>
+        <ul className="list fuel-list">
+          {FUELTYPE_LIST.map(ft => (
+            <li key={ft.id}>{ft.name}</li>
+          ))}
+        </ul>
+      </article>
     </section>
   );
 }
