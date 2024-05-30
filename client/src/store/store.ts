11e160dc6bf840015a505b85cb80afa005481fde
pgw6541 <pgw6541@gmail.com>
2024-05-30 23:11:41 +0900

    Name | toggle 삭제

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,21 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { selectedSeg, selectedFuel, selectedBrand } from "./slice/carFilter";
-import { brandListSlice, toggleSlice } from "./slice/useBrandListSlice";
+import { selectedSeg, selectedFuel, selectedBrand } from "./slice/SelectedSlice";
+import { brandListSlice } from "./slice/useBrandListSlice";
 import { carListSlice, getCarSlice } from "./slice/useCarSlice";
 import { segAll, segCheck, fuelAll, fuelCheck } from "./slice/carCheck-slice";
 import { formDataSlice } from "./slice/createCarSlice";
 
 export const store = configureStore({
   reducer: {
-    selectedSeg: selectedSeg.reducer,
-    selectedFuel: selectedFuel.reducer,
-    selectedBrand: selectedBrand.reducer,
-    toggle: toggleSlice.reducer,
+    
     segAll: segAll.reducer,
     segCheck: segCheck.reducer,
     fuelAll: fuelAll.reducer,
     fuelCheck: fuelCheck.reducer,
-    // Form데이터
+    
+    // 선택 요소 관리
+    selectedSeg: selectedSeg.reducer,
+    selectedFuel: selectedFuel.reducer,
+    selectedBrand: selectedBrand.reducer,
+    
+    // Server, DB
     createCar: formDataSlice.reducer,
     brandList: brandListSlice.reducer,
     carList: carListSlice.reducer,
